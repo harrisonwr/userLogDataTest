ba7a29f37b185b46578a6aec95f465ded0924bff
harrisonwr <harrisonwrc@gmail.com>
2015-11-18 05:09:30 -0800

    Add relation testing code
    Saving and Getting book doesn’t work for now

--- www/js/app.js
+++ www/js/app.js
@@ -46,6 +46,16 @@ app.config(function($stateProvider, $urlRouterProvider) {
     url: '/getData',
     templateUrl: 'templates/getData.html',
     controller: 'DataCtrl'
+  })
+  .state('addBook', {
+    url: '/addBook',
+    templateUrl: 'templates/addBook.html',
+    controller: 'BookCtrl'
+  })
+  .state('getBook', {
+    url: '/getBook',
+    templateUrl: 'templates/getBook.html',
+    controller: 'BookCtrl'
   });
 
   $urlRouterProvider.otherwise("/");
@@ -127,4 +137,69 @@ app.controller('DataCtrl', function($scope, $state){
       }
     });
   }
+});
+
+app.controller('BookCtrl', function($scope, $state){
+  $scope.data = {};
+  $scope.saveBook = function(){
+    console.log($scope.data);
+    var bookData = new Parse.Object("BookDataObject");
+
+    bookData.relation("authors", function(relation) {
+      relation.add($scope.data.authorOne);
+      relation.add($scope.data.authorTwo);
+    });
+
+    bookData.save().then(
+      function(bookData) {
+        // the object was saved.
+        console.log("Save book successfully");
+        console.log(bookData);
+      },
+      function(error) {
+        // saving the object failed.
+        console.log("Error: " + error.code + " " + error.message);
+    });
+
+  };
+
+  $scope.getBook = function(){
+    // var searchName = $scope.data.authorName;
+    //
+    // var bookData = new Parse.Object("BookDataObject");
+    // var relation = bookData.relation("authors");
+    //
+    // var query = relation.query();
+    // console.log(query);
+    // query.find({
+    //   success: function(results) {
+    //     alert("Successfully returned " + results.length + " people!");
+    //     for (var i = 0; i < results.length; i++) {
+    //       var object = results[i];
+    //       console.log(object.id + " - " + object.get("authorOne") + " - " + object.get("authorTwo"));
+    //     }
+    //   },
+    //   error: function(error) {
+    //     alert("Error: " + error.code + " " + error.message);
+    //   }
+    // });
+    var BookData = Parse.Object.extend("BookDataObject");
+    var query = new Parse.Query(BookData);
+
+    query.get("E7aXHh2klo", {
+      success: function(results) {
+        alert("Successfully returned " + results.length + " people!");
+        console.log(results);
+        // for (var i = 0; i < results.length; i++) {
+        //   var object = results[i];
+        //   console.log(object);
+        //   // console.log(object.id + " - " + object.get("authorOne") + " - " + object.get("authorTwo"));
+        // }
+      },
+      error: function(error) {
+        alert("Error: " + error.code + " " + error.message);
+      }
+    });
+
+  };
 });
